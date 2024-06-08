74d42362a27333db4f15da8331e1d3f198bd1ce4
Vlad TBK <most_loginVlad.TBK@proton.me>
2024-06-08 15:03:59 +0300

    updated with svg

--- small-projects/falling-objs/main.js
+++ small-projects/falling-objs/main.js
@@ -2,15 +2,16 @@ import anime from 'animejs/lib/anime.es.js';
 const hex_values = [
     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
 ];
+const pallet_colors = ["#A8A4E6", "#EC776A", "#EE8B67", "#F0B9C2", "#F8E9B4", "#F1CC13", "#8FDF8F", "#858EF7", "#9DE9F7", "#228B22"]
 
-const interval = 1000
+const interval = 500
 let width = null
 let height = null
 
 class Box {
     constructor(bg_color, x_pos) {
         this.element = document.createElement("div")
-        this.element.innerHTML = `<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">            <circle cx="5" cy="5" r="5" fill=${bg_color} />        </svg>`
+        this.element.innerHTML = `<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="5" cy="5" r="5" fill=${bg_color} />        </svg>`
         this.element.className = "box"
         this.element.style.left += x_pos + "px"
         document.body.appendChild(this.element)
@@ -19,7 +20,7 @@ class Box {
     animation() {
         anime({
             targets: this.element,
-            translateY: [-100, height],
+            translateY: [-20, height],
             duration: 100000,
             update: () => {
                 const obj_pos = this.element.getBoundingClientRect()
@@ -33,7 +34,7 @@ class Box {
     }
 }
 
-const get_random_color = () => {
+const generate_random_color = () => {
     let output_color = "#"
     for (let i = 0; i <= 5; ++i) {
         output_color += hex_values[Math.floor(Math.random() * hex_values.length)]
@@ -42,7 +43,7 @@ const get_random_color = () => {
 
 }
 const main = () => {
-    const random_color = get_random_color()
+    const random_color = pallet_colors[Math.floor(Math.random() * pallet_colors.length)]
     const random_x = Math.floor(Math.random() * width)
     new Box(random_color, random_x)
 }
