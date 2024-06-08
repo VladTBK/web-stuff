f15c820e85fbb6f4498df9fa21e55a83b32e7fa6
Vlad TBK <most_loginVlad.TBK@proton.me>
2024-06-08 14:35:51 +0300

    made a particle falling generator

--- small-projects/falling-objs/main.js
+++ small-projects/falling-objs/main.js
@@ -1,21 +1,54 @@
 import anime from 'animejs/lib/anime.es.js';
+const hex_values = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+];
 
-const boxes = document.querySelectorAll('div.box');
-const animation = anime.timeline({
-    loop: true
+const interval = 1000
+let width = null
+let height = null
 
-})
+class Box {
+    constructor(bg_color, x_pos) {
+        this.element = document.createElement("div")
+        this.element.className = "box"
+        this.element.style.left += x_pos + "px"
+        this.element.style.backgroundColor = bg_color
+        document.body.appendChild(this.element)
+        this.animation()
+    }
+    animation() {
+        anime({
+            targets: this.element,
+            translateY: [-100, height],
+            duration: 100000,
+            update: () => {
+                const obj_pos = this.element.getBoundingClientRect()
+                if (obj_pos.top >= height) {
+                    anime.remove(this.element)
+                    this.element.remove()
+                }
 
-for (const box of boxes) {
-    animation.add({
-        targets: box,
-        translateY: [
-            { value: 200, duration: 1000 },
-            { value: 0, duration: 1000 }
-        ],
-        rotate: {
-            value: "1turn",
-            easing: "easeInOutSine"
-        },
-    })
+            }
+        })
+    }
 }
+
+const get_random_color = () => {
+    let output_color = "#"
+    for (let i = 0; i <= 5; ++i) {
+        output_color += hex_values[Math.floor(Math.random() * hex_values.length)]
+    }
+    return output_color
+
+}
+const main = () => {
+    const random_color = get_random_color()
+    const random_x = Math.floor(Math.random() * width)
+    console.log(width)
+    new Box(random_color, random_x)
+}
+document.addEventListener('DOMContentLoaded', function() {
+    width = document.body.scrollWidth
+    height = document.body.scrollHeight
+    setInterval(main, interval)
+});
